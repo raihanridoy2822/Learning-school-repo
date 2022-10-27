e01fada6dcac2067e6fe831a3322fb486d3fed4a
raihanridoy2822 <raihanridoy1894@gmail.com>
2022-10-27 19:12:44 +0600

    home

--- src/page/Home/Home.js
+++ src/page/Home/Home.js
@@ -3,19 +3,19 @@ import { useLoaderData } from 'react-router';
 import Newcard from '../../Shared/Newcard/Newcard';
 
 const Home = () => {
-    const courses=useLoaderData();
+    const courses = useLoaderData();
     return (
-        <div > 
-            <h1>This is home component:{courses.length}</h1>
-            <div  className="row row-cols-1 row-cols-md-2 g-4">
-                 {
-                courses.map(course=><Newcard 
-                    key={courses._id}
-                    course={course}
-                ></Newcard>)
-            }
+        <div >
+            <h1>This is a home component:{courses.length}</h1>
+            <div className="row row-cols-1 row-cols-md-2 g-4">
+                {
+                    courses.map(course => <Newcard
+                        key={courses._id}
+                        course={course}
+                    ></Newcard>)
+                }
             </div>
-           
+
         </div>
     );
 };
