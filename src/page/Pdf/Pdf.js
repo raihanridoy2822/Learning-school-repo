737bc4dc41b546d910b26d49a38159d554b3cafb
raihanridoy2822 <raihanridoy1894@gmail.com>
2022-10-27 19:14:16 +0600

    pdf

--- src/page/Pdf/Pdf.js
+++ src/page/Pdf/Pdf.js
@@ -6,18 +6,18 @@ import Pdf from "react-to-pdf";
 const ref = React.createRef();
 
 const Pdfg = () => {
-    return (
-        <div>
-            <div className="App">
-      <Pdf targetRef={ref} filename="code-example.pdf">
-        {({ toPdf }) => <button onClick={toPdf}> Pdf Link</button>}
-      </Pdf>
-      <div ref={ref}>
-       Please Click This Button
+  return (
+    <div>
+      <div className="App">
+        <Pdf targetRef={ref} filename="code-example.pdf">
+          {({ toPdf }) => <button onClick={toPdf}> Pdf Link</button>}
+        </Pdf>
+        <div ref={ref}>
+          Please Click This a Button
+        </div>
       </div>
     </div>
-        </div>
-    );
+  );
 };
 
 export default Pdfg;
